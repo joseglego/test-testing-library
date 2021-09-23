97054c5a57fe3ab6a12ad349b5768a26aa0282e9
José Lezama <jose.lezama@clevertech.biz>
2021-09-23 15:54:18 -0300

    core(test): mock react-18next while testing

--- src/RenderPressMedia.test.js
+++ src/RenderPressMedia.test.js
@@ -1,6 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import RenderPressMedia from './RenderPressMedia';
 
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+}));
+
  const data = [
   {
    id: 65,
