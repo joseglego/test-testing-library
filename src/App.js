b4e1d37c1eed2ba13345476547859a49c0947615
José Lezama <jose.lezama@clevertech.biz>
2021-09-23 15:28:20 -0300

    core(): install dependencies and configure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
 import logo from './logo.svg';
 import './App.css';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources: {
+      en: {
+        translation: {
+          "Welcome to React": "Welcome to React and react-i18next",
+          press: {
+            text: "files {{media}}",
+            media: {
+              phone: " by phone",
+              letter: "bu letter",
+              email: "bu email"
+            }
+          }
+        }
+      }
+    },
+    lng: "en", // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    }
+  });
 
 function App() {
   return (
